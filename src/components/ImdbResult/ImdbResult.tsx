15f5d50063574f29f240db9b27cb345007765876
Matteo Kinkela <matteo.kinkela@q-software.com>
2020-09-22 22:14:09 +0200

    feat(imdb-result): add imdb response to ImdbResult component

--- src/components/ImdbResult/ImdbResult.tsx
+++ src/components/ImdbResult/ImdbResult.tsx
@@ -1,5 +1,11 @@
 import React, {FunctionComponent, useContext} from "react";
-import Paper from "@material-ui/core/Paper";
+import {
+  Paper,
+  Table,
+  TableRow,
+  TableCell,
+  List, ListItemText, ListItem, Typography
+} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 import {ImdbContext} from "../../contexts/ImdbContext";
 
@@ -11,6 +17,12 @@ const useStyles = makeStyles((theme) => ({
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
+  listItem: {
+    borderBottom: '1px solid #cdcdcd',
+    '&:last-child': {
+      borderBottom: 'none'
+    }
+  }
 }));
 
 const ImdbResult: FunctionComponent<ImdbResultProps> = () => {
@@ -18,9 +30,48 @@ const ImdbResult: FunctionComponent<ImdbResultProps> = () => {
   const { movie } = useContext(ImdbContext);
 
   const classes = useStyles();
+  const removeKeys = ['Poster', 'Response', 'DateRequested', 'Ratings']
+
+  const generateTable = () => {
+    if (movie)
+      return Object.keys(movie).filter(key => !removeKeys.includes(key)).map(key => (
+        <TableRow>
+          <TableCell>{key}</TableCell>
+          <TableCell>
+            {
+              // @ts-ignore
+              movie[key]
+            }
+          </TableCell>
+        </TableRow>
+      ));
+  }
 
   return (
-    <Paper className={classes.paper}>{movie?.Title}</Paper>
+    <Paper className={classes.paper}>
+      {!movie ?
+        '' :
+        <>
+          <img src={movie.Poster} alt="Movie poster" />
+          <Table>
+            {generateTable()}
+          </Table>
+          <Typography variant="subtitle1">
+            Ratings
+          </Typography>
+          <List>
+            {!movie.Ratings?.length ?
+              '' :
+              movie.Ratings.map(({Source, Value}, idx: number) => (
+                <ListItem key={idx} className={classes.listItem}>
+                  <ListItemText primary={Source} secondary={Value} />
+                </ListItem>
+              ))
+            }
+          </List>
+        </>
+      }
+    </Paper>
   );
 }
 
